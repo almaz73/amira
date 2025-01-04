c70943c1f8294a32495e68d3e6fe2e554e3bf9d7
almaz <almaz_73@mail.ru>
2025-01-04 13:15:53 +0300

    слова для игры

--- pyGame/main.py
+++ pyGame/main.py
@@ -1,26 +1,36 @@
 import pygame
 import sys
+import time
+import words
+import text_1
 
 pygame.init()
 pygame.display.set_caption('Моя Первая Игра на питоне')  # заголовок
-img = pygame.image.load('icon.png')  # рисунок иконки
+img = pygame.image.load('pyGame/icon.png')  # рисунок иконки
 pygame.display.set_icon(img)  # закрепляем иконку
 screen = pygame.display.set_mode((800, 500))  # размеры игрового поля
 font = pygame.font.SysFont('comicsansms', 32)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
-follow = font.render('Подписывайся на канал', 1, RED, GREEN)
+follow = font.render('Найди пару', 1, RED, GREEN)
 like = font.render('Ставь лайк', 1, GREEN, BLUE)
 
 r = pygame.Rect(50, 50, 100, 200)
 pygame.draw.rect(screen, (255, 0, 0), r, 0)
+
+print('- - - - -', text_1.text.split('\n')[0])
+
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-    screen.blit(follow, (0,0))
+    screen.blit(follow, (300,20))
     screen.blit(like, (100,100))
     pygame.display.flip()
+    time.sleep(.5)
+
+
+
---
+++ pyGame/words.py
@@ -0,0 +1,7 @@
+def getList():
+  a = [{'Привет':'Hi!'}, {'Fell':'Упасть'}, {'Snile':'Улыбка'} ]
+  return a
+
+def getList():
+  a = ['Привет', 'Hi!', 'Чао!']
+  return a
