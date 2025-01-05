c13916adb9c82ba7e8753bd38271122847bce4c9
almaz <almaz_73@mail.ru>
2025-01-05 14:53:19 +0300

    временное хранилище

--- pyGame/main.py
+++ pyGame/main.py
@@ -1,11 +1,11 @@
 import pygame
 import sys
-import time
 import words
-import text_1
+
 
 pygame.init()
-pygame.display.set_caption('Моя Первая Игра на питоне')  # заголовок
+clock = pygame.time.Clock()
+pygame.display.set_caption('Запоминаем 1000 часто используемых английских слов')  # заголовок
 img = pygame.image.load('pyGame/icon.png')  # рисунок иконки
 pygame.display.set_icon(img)  # закрепляем иконку
 screen = pygame.display.set_mode((800, 500))  # размеры игрового поля
@@ -14,23 +14,39 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 follow = font.render('Найди пару', 1, RED, GREEN)
-like = font.render('Ставь лайк', 1, GREEN, BLUE)
 
-r = pygame.Rect(50, 50, 100, 200)
-pygame.draw.rect(screen, (255, 0, 0), r, 0)
+screen.blit(follow, (300,20))
 
-print('- - - - -', text_1.text.split('\n')[0])
+
+
+
+word4 = words.getWords()
+print(word4)
+
+
+# расставит 4 пары слов
+def setPlace(words):
+    eng=[]
+    ru=[]
+    print(len(words))
+    for i in words:
+        eng.append(font.render('    '+i[0]+'  ', 1, GREEN, BLUE))
+        ru.append(font.render( i[1]+'  ', 1, GREEN, BLUE))
+    for r in range(len(words)):
+        screen.blit(eng[r], (350-eng[r].get_width(),100+r*60))
+        screen.blit(ru[r], (420,100+r*60))
 
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
-            sys.exit()
-    screen.blit(follow, (300,20))
-    screen.blit(like, (100,100))
+            sys.exit()    
+    setPlace(word4)
     pygame.display.flip()
-    time.sleep(.5)
+    clock.tick(1)
+    
+    #time.sleep(2)
 
 
 
--- pyGame/words.py
+++ pyGame/words.py
@@ -1,7 +1,29 @@
-def getList():
-  a = [{'Привет':'Hi!'}, {'Fell':'Упасть'}, {'Snile':'Улыбка'} ]
-  return a
+import json
+import text_1
 
-def getList():
-  a = ['Привет', 'Hi!', 'Чао!']
-  return a
+# сохранение переменной
+data = {'learned':[1,2,3], 'needRepeat':[]}
+print(data)
+with open('data.json', 'w', encoding='utf-8') as f:    
+    json.dump(data, f, ensure_ascii=False, indent=4)
+
+with open('data.json', 'r') as f:
+    loaded_data = json.load(f)
+    print(loaded_data)
+    print(loaded_data['learned'])
+    print(loaded_data['learned'][0])
+    print(loaded_data['needRepeat'])
+
+# получаем список слов случайным образом из списка 998 вариантов часто использщуемых 
+def getWords():
+    # print(len(text_1.text.split('\n')))
+    # print('===',range(len(text_1.text.split('\n'))))
+    return [ 
+      text_1.text.split('\n')[2].split('—'),
+      text_1.text.split('\n')[22].split('—'),
+      text_1.text.split('\n')[332].split('—'),
+      text_1.text.split('\n')[42].split('—'),
+      text_1.text.split('\n')[23].split('—')
+    ]
+
+print(getWords())
